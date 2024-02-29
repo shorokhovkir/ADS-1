2ca0ac57acc81788221ea0955344acf33e35ffab
shorokhovkir <160240169+shorokhovkir@users.noreply.github.com>
2024-02-29 12:23:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,33 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  for (uint64_t i = 2; i < value; i++)
+    if (value % i == 0)
+      return false;
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  uint64_t* array = new uint64_t[n]{};
+  uint64_t counter = 0;
+  for (uint64_t i = 2; array[n - 1] == 0; i++)
+    if (checkPrime(i))
+      array[counter++] = i;
+  uint64_t res = array[n - 1];
+  delete[] array;
+  return res;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  for (uint64_t i = value + 1; ; i++)
+    if (checkPrime(i))
+      return i;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t res = 0;
+  for (uint64_t i = 2; i < hbound; i++)
+    if (checkPrime(i))
+      res += i;
+  return res;
 }
